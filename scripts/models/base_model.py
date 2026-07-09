2246debd564cd3d647964d5310f4ff37e1ecd3e7
Aman Singh <singhaman5467@gmail.com>
2026-07-10 00:17:57 +0545

    Implement persistence baseline and reusable Basemodel Architecture

---
+++ scripts/models/base_model.py
@@ -0,0 +1,111 @@
+import pandas as pd
+from logger import logger
+from sklearn.metrics import(
+    mean_absolute_error,
+    mean_squared_error,
+    r2_score,
+)
+from config import(
+    TEST_DIR
+)
+
+class BaseModel:
+
+    def __init__(self, results_dir):
+
+        self.results_dir = results_dir
+
+        self.results = []
+
+        self.results_dir.mkdir(
+            parents=True,
+            exist_ok=True,
+        )
+    def load_dataset(self, csv_file):
+
+        return pd.read_csv(csv_file)
+    
+    def prepare_features(self, df):
+
+        X = df.drop(
+            columns=[
+                "timestamp",
+                "target_pm2_5",
+            ]
+        )
+
+        y = df["target_pm2_5"]
+
+        return X, y
+    
+    def evaluate(self, target, prediction):
+
+        mae = mean_absolute_error(
+            target,
+            prediction,
+        )
+
+        rmse = (
+            mean_squared_error(
+                target,
+                prediction,
+            ) ** 0.5
+        )
+
+        r2 = r2_score(
+            target,
+            prediction,
+        )
+
+        return mae, rmse, r2
+    
+    def save_results(self):
+
+        results_df = pd.DataFrame(
+            self.results
+        )
+
+        results_df.to_csv(
+
+            self.results_dir /
+            "metrics.csv",
+
+            index=False,
+        )
+
+    def print_summary(self):
+
+        results_df = pd.DataFrame(self.results)
+        logger.info("=" * 50)
+        logger.info("Overall Persistence Baseline")
+        logger.info("=" * 50)
+
+        logger.info(
+            f"Average MAE  : {results_df['mae'].mean():.3f}"
+        )
+
+        logger.info(
+            f"Average RMSE : {results_df['rmse'].mean():.3f}"
+        )
+
+        logger.info(
+            f"Average R²   : {results_df['r2'].mean():.3f}"
+        )
+
+    def run(self):
+
+        csv_files = sorted(
+            TEST_DIR.glob("*.csv")
+        )
+
+        logger.info(
+            f"Evaluating {len(csv_files)} stations..."
+        )
+
+        for csv_file in csv_files:
+
+            self.evaluate_station(csv_file)
+
+        self.save_results()
+
+        self.print_summary()
--- scripts/models/persistence.py
+++ scripts/models/persistence.py
@@ -1,18 +1,16 @@
 import pandas as pd
 from logger import logger
+from models.base_model import BaseModel
 from config import(
-    TEST_DIR,
-    PERSISTENCE_RESULTS_DIR,
+    PERSISTENCE_RESULTS_DIR
 )
-from sklearn.metrics import (
-            mean_absolute_error,
-            mean_squared_error,
-            r2_score,
-        )
-class PersistenceModel:
+
+class PersistenceModel(BaseModel):
 
     def __init__(self):
-        self.results = []
+        super().__init__(
+            PERSISTENCE_RESULTS_DIR
+        )
 
     def predict(self, df):
 
@@ -20,26 +18,14 @@ class PersistenceModel:
         
     
     def evaluate_station(self,csv_file):
-        test_df = pd.read_csv(csv_file)
-        predictions = self.predict(test_df)
+        test_df = self.load_dataset(csv_file)
+        prediction = self.predict(test_df)
 
         target = test_df["target_pm2_5"]
 
-        mae = mean_absolute_error(
+        mae, rmse, r2 = self.evaluate(
             target,
-            predictions,
-        )
-
-        rmse = (
-            mean_squared_error(
-                target,
-                predictions,
-            ) ** 0.5
-        )
-
-        r2 = r2_score(
-            target,
-            predictions,
+            prediction,
         )
 
         self.results.append({
@@ -54,50 +40,4 @@ class PersistenceModel:
 
         })
 
-    def save_results(self):
-        results_df = pd.DataFrame(self.results)
-
-        results_df.to_csv(
-
-            PERSISTENCE_RESULTS_DIR /
-            "metrics.csv",
-
-            index=False,
-
-        )
-
-    def print_summary(self):
-        results_df = pd.DataFrame(self.results)
-        logger.info("=" * 50)
-        logger.info("Overall Persistence Baseline")
-        logger.info("=" * 50)
-
-        logger.info(
-            f"Average MAE  : {results_df['mae'].mean():.3f}"
-        )
-
-        logger.info(
-            f"Average RMSE : {results_df['rmse'].mean():.3f}"
-        )
-
-        logger.info(
-            f"Average R²   : {results_df['r2'].mean():.3f}"
-        )
-
-    def run(self):
-
-        csv_files = sorted(
-            TEST_DIR.glob("*.csv")
-        )
-
-        logger.info(
-            f"Evaluating {len(csv_files)} stations..."
-        )
-
-        for csv_file in csv_files:
-
-            self.evaluate_station(csv_file)
-
-        self.save_results()
-
-        self.print_summary()
+    
